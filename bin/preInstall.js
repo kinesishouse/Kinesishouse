f05f774bbe73094226b6ced3546ba7b1331ebe47
Deyual Simu <deyual@gmail.com>
2019-07-07 00:07:47 -0400

    Add env to service

--- bin/preInstall.js
+++ bin/preInstall.js
@@ -4,7 +4,11 @@ const Service = require('node-linux').Service;
 const svc = new Service({
     name:'KinesisHouse',
     description: 'KinesisHouse web server.',
-    script: '/srv/KinesisHouse/bin/www'
+    script: '/srv/KinesisHouse/bin/www',
+    env: [{
+        name: "PORT",
+        value: 80
+    }]
 });
 
 // Listen for the "install" event, which indicates the

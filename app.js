57b8ca9b4ce6bdfbb66374aea1c32271ceca58d7
Deyual Simu <deyual@gmail.com>
2019-07-13 14:20:26 -0400

    add sitemap and more

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const compression = require('compression');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const favicon = require('serve-favicon');
+
+
 
 const indexRouter = require('./routes');
 
@@ -20,6 +23,7 @@ app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(favicon(__dirname + '/public/images/Favicon.webp'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {

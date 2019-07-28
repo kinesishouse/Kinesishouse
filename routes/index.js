873782ee4959b55f5e997fa6322002eec5583e75
paulo <kinesishouse@gmail.com>
2019-07-28 18:26:40 -0400

    correccion router

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,9 @@ router.get('/', function (req, res) {
 router.get('/profesional-kinesiologo', function (req, res, next) {
   res.render('profesional-kinesiologo', {
     pageTitle: 'kinesiologo profesional | kinesiologia a domicilio',
-    images: 'images'
-    metaDescripcion: ' curriculum de kinesiologo profesional que atiende en santiago de chile atencion especial en todas las comunas de la region metropolitana'
-    tituloH1: 'curriculum de licenciado en kinesiologia',
+    images: 'images',
+    metaDescripcion: ' curriculum de kinesiologo profesional que atiende en santiago de chile atencion especial en todas las comunas de la region metropolitana',
+    tituloH1: 'curriculum de licenciado en kinesiologia'
   })
 }); 
 module.exports = router;

720e9713af06ab3a46dda7ff9117194928010859
paulo <kinesishouse@gmail.com>
2020-01-28 13:23:43 -0300

    arreglos en js

---
+++ public/js/funciones.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //var elems = document.querySelectorAll('.sidenav');
+    //var instances = M.Sidenav.init(elems);
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems);
+    M.AutoInit();
+  });

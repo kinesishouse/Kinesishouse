26f368aa5f29c82f9e9e21d6530f1cd4167774ed
paulo <kinesishouse@gmail.com>
2020-01-31 09:02:38 -0300

    arreglos en rendimiento

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res) {
   res.render('index', {
-      pageTitle: 'Kinesiologo a domicilio ',
+      pageTitle: 'Kinesiologo a domicilio en Santiago ',
       images: 'images',
       metaDescripcion: 'servicio de kinesiologia a domicilio para las comunas de : Las Condes, Providencia, La Florida, Macul, Peñalolen y Ñuñoa.',
-      tiTuloh1: 'kinesiologo a domicilio Santiago',
+      tiTuloh1: 'Kinesiologo a domicilio Santiago',
       index: 'index',
       canonical: 'http://www.kinesishouse.cl/'
   })

38afbe10e961ef1f947545b8fe4d9deb71c6bc00
paulo <kinesishouse@gmail.com>
2020-02-13 02:34:56 -0300

    arreglo en diseño global

---
+++ public/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.modal').modal();
+    $('.materialboxed').materialbox();
+    $(".dropdown-trigger").dropdown();
+    $('.fixed-action-btn').floatingActionButton();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space

606f2a9145ce5f786a47bfe4154f528008f377d2
paulo <kinesishouse@gmail.com>
2020-02-13 16:00:01 -0300

    arreglos en link 48px

--- public/js/funciones.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-   $('.modal').modal();
-  $('.sidenav').sidenav();
-  $('.materialboxed').materialbox();
-  $('parallax').parallax();
-  $(".dropdown-trigger").dropdown();
-  $('.fixed-action-btn').floatingActionButton();
-
-});

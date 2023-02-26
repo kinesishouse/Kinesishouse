3fa46d418f540f1a61ddff070441f93036501aff
paulo <kinesishouse@gmail.com>
2023-02-26 12:40:22 -0300

    arreglos en parametros de schemas.org

--- routes/index.js
+++ routes/index.js
@@ -72,13 +72,13 @@ router.get('/about', function (req, res) {
       pageTitle: 'kinesiologo respiratorio a domicilio Santiago ',
       images: 'images',
       metaDescripcion: ' ▷ servicio de kinesiologia respiratoria a domicilio en santiago de chile, neumonia, bronquitis, epoc, aspiracion de secreciones',
-      tiTuloh1: 'kinesiologo respiratorio a domicilio,',
+      tiTuloh1: 'kinesiologo respiratorio a domicilio',
       index: 'index',
       canonical: 'http://www.kinesishouse.cl/kinesiologo-respiratorio'
     });
   }); 
-  router.get('/kinesiologo-respiratorio/kinesiologia-respiratoria-infantil', function (req, res) {
-    res.render('./kinesiologia-respiratoria-infantil', {
+  router.get('/kinesiologo-respiratorio/kinesiologo-respiratorio-infantil', function (req, res) {
+    res.render('./respiratorio/kinesiologo-respiratorio-infantil', {
       pageTitle: 'kinesiologo respiratorio infantil a domicilio  ',
       images: 'images',
       metaDescripcion: 'kinesiologia respiratoria niños a domicilio en santiago de chile, asma, bronquitis, neumonia, fibrosis quistica, sindrome bronquial obstructivo',
@@ -239,7 +239,7 @@ router.get('/about', function (req, res) {
     });
   }); 
    router.get('/kinesiologo-adulto-mayor', function (req, res) {
-    res.render('./adultomayor', {
+    res.render('./adulto-mayor', {
       pageTitle: 'kinesiologo para adulto mayor | geriatrico | a domicilio Santiago ',
       images: 'images',
       metaDescripcion: '▷ servicio de kinesiologia a domicilio en adulto mayor, ejercicios, kinesiologia integral, postrados, post operados, alzeimer, parkinson, sedentarismo.',
@@ -259,7 +259,7 @@ router.get('/about', function (req, res) {
     });
   }); 
    router.get('/kinesiologo-deportivo', function (req, res) {
-    res.render('./kinedeportiva', {
+    res.render('./deportivo', {
       pageTitle: 'kinesiologo deportivo | kinesiologia | a domicilio Santiago ',
       images: 'images',
       metaDescripcion: '▷ servicio de kinesiologia a domicilio deportiva, ejercicios, kinesiologia integral, lesiones deportivas, lesiones de running.',
